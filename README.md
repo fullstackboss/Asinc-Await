2e8427a4c85b502ef4978a022ebe5901aa5707cd
fullstackboss <68229141+fullstackboss@users.noreply.github.com>
2020-11-29 08:58:59 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Asinc-Await
+# prueba
 Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,27 @@
+import "./styles.css";
+
+fetch("https://pokeapi.co/api/v2/pokemon/")
+  .then((datos) => {
+    return datos.json();
+  })
+  .then((otros) => {
+    // console.log(otros.results)
+    otros.results.forEach((element) => {
+      // console.log(element.name);
+    });
+  })
+  .catch((error) => console.log(error));
+
+//asyn await
+const consultaDatos = async () => {
+  try {
+    const pidedatos = await fetch("https://pokeapi.co/api/v2/pokemon/");
+    const misdatos = await pidedatos.json();
+    const miarreglo = misdatos.results.map((poke) => poke.name);
+    console.log(miarreglo);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+consultaDatos();
